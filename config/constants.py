16121009487ec9d8de8978b9ce10517c77b33585
Tonatiu <yahuitl.trejo@gmail.com>
2016-11-07 22:08:31 -0600

    El paquete contiene los valores de las mibs que son monitoreadas

---
+++ config/constants.py
@@ -0,0 +1,14 @@
+sysDscr = ["1.3.6.1.2.1.1.1.0"]
+"""totalRam = "1.3.6.1.4.1.2021.4.5.0"
+ramUsed = "1.3.6.1.4.1.2021.4.6.0"
+ramFree = "1.3.6.1.4.1.2021.4.11.0"
+sysCPUsed = "1.3.6.1.4.1.2021.11.10.0"""
+sysUpTime = "1.3.6.1.2.1.1.3.0"
+udpInDatagrams = "1.3.6.1.2.1.7.1.0"
+udpInErrors = "1.3.6.1.2.1.7.3.0"
+tcpErrorsRecibed = "1.3.6.1.2.1.6.14.0"
+tcpSegmentRecibed = "1.3.6.1.2.1.6.10.0"
+snmpOutGetResponses  = "1.3.6.1.2.1.11.28.0"
+
+
+mibObjects = [sysUpTime, udpInDatagrams, udpInErrors, tcpErrorsRecibed, tcpSegmentRecibed, snmpOutGetResponses]
